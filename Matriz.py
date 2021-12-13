a72074bffebc0d9871d7488cea1cb581f880570a
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-13 17:53:17 +0100

    2

---
+++ Matriz.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+int sum = 0;
+for (int = 0; i < ar.Length; i ++ )
+{
+    sum += 0;
+}
+return sum;
+# Write your code here
+
+if __name__ == '__main__':
+fptr = open(os.environ['OUTPUT_PATH'], 'w')
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+result = simpleArraySum(ar)
+fptr.write(str(result) + '\n')
+fptr.close()
