6ffb7c2e7d02382a290828d8e3e94cb392af0b79
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-18 23:09:33 +0100

    segunda parte rana

--- rana.py
+++ rana.py
@@ -12,3 +12,19 @@ class Coordenadas:
             return True
         else: 
             return False
+
+class Tunel:
+    def __init__(self,x1,x2,y1,y2):
+        self.extremo1 = Coordenadas(x1,y1)
+        self.extremo2 = Coordenadas (x2,y2)
+
+def BuscaTunel (casillax, casillay, tuneles):
+    coordenadas = Coordenadas(casillax, casillay)
+    for t in tuneles:
+        if t.extremo1.comparate(casillax,casillay)==True:
+            coordenadas.x = t.extremo2.x
+            coordenadas.y = t.extremo2.y
+        elif t.extremo2.comparate(casillax, casillay):
+            coordenadas.x=t.extremo1.x
+            coordenadas.y = t.extremo1.y
+    return coordenadas
