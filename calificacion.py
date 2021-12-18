6889de611a8a5610b76c8fa1dc62eaaa0a7574fd
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-18 23:01:01 +0100

    segunda parte calificación

--- calificacion.py
+++ calificacion.py
@@ -8,3 +8,16 @@ def CalificacionesEstudiantes(notas):
     for i in notas:
         lista1.append(notafinal(i))
     return lista1
+
+def CalificacionFinal(notas):
+    notaentera = 0
+    if notas < 40:
+        notaentera = notas
+    else:
+        quebrados= int(notas / 5 + 1)
+        factor= int(cocientes * 5)
+        if factor - notas < 3:
+            notaentera = factor
+        else:
+            notarentera= notas
+    return notaentera
