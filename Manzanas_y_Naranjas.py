d4f6ee93034505ebfd93343697ed93356cd06be8
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-18 22:56:43 +0100

    segunda parte m y n

--- Manzanas_y_Naranjas.py
+++ Manzanas_y_Naranjas.py
@@ -10,3 +10,23 @@ def contador(a, b, c, d, manzanas, naranjas):
     for naranja1 in naranjas:
         if d + naranja1 >= a and d <= b:
             num_naranjas+= 1
+
+            print("Han entrado ", str(num_manzanas), " manzanas")
+        print("Han entrado ", str(num_naranjas), " naranjas")
+
+
+    primer = (
+        input(
+        "Introduzca dos numeros que serán la amplitud de la casa: "
+    )
+    .rstrip()
+    .split()
+    )
+    segundo = (
+        input(
+        "Introduzca dos números que serán la distancia entre los dos árboles: "
+    )
+    .rstrip()
+    .split()
+    )
+    tercero = input("Introduzca dos números: ").rstrip().split()
