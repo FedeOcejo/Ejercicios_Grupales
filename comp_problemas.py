f11e10472c830ae5bf49905eca0f1a7353c22475
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-18 19:34:28 +0100

    correcion del problema

--- comp_problemas.py
+++ comp_problemas.py
@@ -11,69 +11,28 @@ lucia2=randint (1,100)
 
 lucia3=randint (1,100)
 
-lucia4=randint (1,100)
-
-lucia5=randint (1,100)
-
-lucia6=randint (1,100)
-
-lucia7=randint (1,100)
-
-lucia8=randint (1,100)
-
-lucia9=randint (1,100)
-
 carlos1=randint (1,100)
 
 carlos2=randint (1,100)
 
 carlos3=randint (1,100)
 
-carlos4=randint (1,100)
-
-carlos5=randint (1,100)
-
-carlos6=randint (1,100)
-
-carlos7=randint (1,100)
-
-carlos8=randint (1,100)
-
-carlos9=randint (1,100)
 
 print("Lucia en la primera acividad saco: ")
 print("\nClarity={}".format(lucia1))
 print("\nOriginality={}".format(lucia2))
 print("\nDificulty={}".format(lucia3))
 
-print("Lucia en la segunda acividad saco: ")
-print("\nClarity={}".format(lucia4))
-print("\nOriginality={}".format(lucia5))
-print("\nDificulty={}".format(lucia6))
-
-print("Lucia en la tercera acividad saco: ")
-print("\nClarity={}".format(lucia7))
-print("\nOriginality={}".format(lucia8))
-print("\nDificulty={}".format(lucia9))
 
 print("Carlos en la primera acividad saco: ")
 print("\nClarity={}".format(carlos1))
 print("\nOriginality={}".format(carlos2))
 print("\nDificulty={}".format(carlos3))
 
-print("Carlos en la segunda acividad saco: ")
-print("\nClarity={}".format(carlos4))
-print("\nOriginality={}".format(carlos5))
-print("\nDificulty={}".format(carlos6))
 
-print("Carlos en la primera acividad saco: ")
-print("\nClarity={}".format(carlos7))
-print("\nOriginality={}".format(carlos8))
-print("\nDificulty={}".format(carlos9))
+def compareTipelts():
+    pointsLU=0
+    pointsCA=0
 
-
-
-def compareTipelts()
-pointsLU=0
-pointsCA=0
-
+    if lucia1 > carlos1:
+        pointsCA+=1
