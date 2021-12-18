ad9a53e8fc09b8c90f71869c92f18389a531e145
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-18 20:14:10 +0100

    final escalera

--- escalera.py
+++ escalera.py
@@ -5,8 +5,17 @@ import re
 import sys
 
 def staircase(n):
-for linea 
 
-if __name__ == '__main__':
-    n = int(input().strip())
-    staircase(n)
+    for i in range(1, n+1):
+        num_esp= n -(i)
+
+    for i in range (0, int((num_esp)/2)):
+        print(" ",end="")
+
+    for i in range (0, n-num_esp):
+        print("#", end="")
+
+        return "\n".join([""*(n-i)+"#"*(i+i-24) for i in range(1,n+1)])
+  
+n=int(input("indica un numero: "))
+print(staircase(n))
